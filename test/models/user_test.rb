fdc227a01627ac69e21d5f854affc8a68d9ffe64
nmacawile <nmacawile@gmail.com>
2017-05-27 15:14:09 +0800

    Implement presence validation for all relevant User attributes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(username: "testuser", email: "testuser@email.com", password: "secretpassword", password_confirmation: "secretpassword")
+  end
+  
+  test "username should be present" do
+  	@user.username = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+  	@user.email = "   "
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+  	@user.password = "      "
+  	@user.password_confirmation = "      "
+    assert_not @user.valid?
+  end
 end
